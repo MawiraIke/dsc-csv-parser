9db64ae875b7b42dead789d52199f58ff1089fae
MawiraIke <mawiraike@gmail.com>
2020-01-02 16:06:11 +0300

    Formatting
    Signed-off-by: MawiraIke <mawiraike@gmail.com>

--- src/csv_parser/core.clj
+++ src/csv_parser/core.clj
@@ -11,14 +11,10 @@
   (if (nil? pre) 0 (inc pre)))
 
 (defn parse-data []
-  (let [data (get-data)
-        rest (rest data)
-        acc (atom {})]
+  (let [data (get-data) rest (rest data) acc (atom {})]
     (dosync
       (doseq [x rest]
         (let [choices (nth x 4)]
           (if (clojure.string/includes? choices ";")
             (doseq [y (clojure.string/split choices #";")]
-              (swap! acc update y custom-inc))
-            (swap! acc update choices custom-inc))))
-      @acc)))
+              (swap! acc update y custom-inc)) (swap! acc update choices custom-inc)))) @acc)))
